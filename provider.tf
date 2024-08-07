6b9316d8e646bc9ebe406820574d62ab4e32260e
brother <brother@pury.com>
2024-08-08 00:19:04 +0530

    added backend file and removed secrets

--- provider.tf
+++ provider.tf
@@ -1,8 +1,13 @@
 provider "azurerm" {
   features {}
+}
 
-  client_id       = "ba186f2f-c274-4962-936f-6a0f057fb3ee"
-  client_secret   = "gH78Q~3WIiU46vvcKfUhcNT030_c2yJHrm~4ZawR"
-  tenant_id       = "85c3d96d-a33b-4c73-bbff-004065846664"
-  subscription_id = "6d06d021-b278-40e1-ac9e-1a2639091921"
+terraform {
+  backend "azurerm" {
+    # resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "pipesta"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "container1"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    access_key           = "uyk07G5fjeCLJldb1QshLy5PnkBwapl5M3ncfSqPU7H6uBWTnoZw2tVSDz2yZ8y4nmESZfG/PUof+AStQ0caHA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+  }
 }
