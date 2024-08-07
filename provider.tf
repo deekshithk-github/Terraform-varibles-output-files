0c1cdc28e9fd5180f1fdb62a4c7fb8e496461155
brother <brother@pury.com>
2024-08-08 00:25:36 +0530

    removed container secret

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
     storage_account_name = "pipesta"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "container1"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    access_key           = "uyk07G5fjeCLJldb1QshLy5PnkBwapl5M3ncfSqPU7H6uBWTnoZw2tVSDz2yZ8y4nmESZfG/PUof+AStQ0caHA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    access_key           = # Can also be set via `ARM_ACCESS_KEY` environment variable.
   }
 }
