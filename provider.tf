31edb386d893b594a990409ae6234997282c96c4
jenkins-examples <81957488+deekshithk-github@users.noreply.github.com>
2024-09-09 21:03:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,9 +5,9 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {    
     # resource_group_name  = "StorageAccount-ResourceGroup"                     
-    storage_account_name = "tfstaproject01"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "tfstaproject011"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "container1"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    access_key           = "YoZO9CdY22hvHOmBJ3uX37StXuovFQsAQC6EgIxfIjr8indz/LOFO87Oe3dfpRt476wKlAmy25Xn+AStYoqpLA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
+    access_key           = "IWQFsfkuuMrc/K1N7mcvliQQpYjLSSvgT4BCT1xNh/X0yyUBhr3vlIIgGtiZpQC1ytiT3+uWO+pG+AStiHH6mA=="  # Can also be set via `ARM_ACCESS_KEY` environment variable.
   }
 }
